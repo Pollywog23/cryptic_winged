93703d9cbc5dc32fa3c3d67705497ab45f5f2398
Marsha Clark <marshasclark23@gmail.com>
2015-10-02 12:56:57 -0500

    All working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -150,3 +150,6 @@ function main() {
 }
 main();
 
+
+
+
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module CrypticWinged
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.default_url_options = { host: 'crypticwingedphotography@yahoo.com' }
   end
 end
