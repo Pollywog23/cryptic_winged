9a93e1abc48004bec80c52aabbb5e5e960040d23
Marsha Clark <marshasclark23@gmail.com>
2015-10-08 13:14:36 -0500

    mailer change

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,13 +1,13 @@
 class AdminMailer < ApplicationMailer
 
-  default from: "marshasclark23@gmail.com"
+  default from: "crypticwingedphotography@yahoo.com"
 
   def contact_email(name, email, phone, body)
     @name = name
     @email = email
     @phone = phone
     @body = body
-    mail(to:'crypticwingedphotography@yahoo.com
+    mail(to:'crypticwingedphotography@yahoo.com')
   end
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['crypticwinged@yahoo.com'],
+   :user_name            => ENV['crypticwingedserver@gmail.com'],
    :password             => ENV['ShawnnaJones2012'],
    :authentication       => "plain",
   :enable_starttls_auto => true
